ca246e86d384803bda4035dbbaa75dd2dba28e71
diferoca1978 <rodriguezcastrodiegof@gmail.com>
2025-07-14 17:05:17 -0500

    Feat: "Update navigation and global styles
    - Enhanced NavBar component with new navigation structure
    - Updated global styles for better consistency
    - Improved navigation utility functions.

---
+++ src/utils/navigation.ts
@@ -0,0 +1,8 @@
+export const navBarLinksConst = [
+  { name: "Inicio", path: "/" },
+  { name: "Nosotros", path: "/nosotros" },
+  { name: "Servicios", path: "/servicios" },
+  { name: "Contacto", path: "/contacto" },
+  { name: "Historias de Exito", path: "/historiasDeExito" }
+  // { name: "Blog", path: "/blog" },
+] as const;
