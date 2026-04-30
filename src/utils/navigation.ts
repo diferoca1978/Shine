9aab9c5535be40439d120c93a690b79a35e7af10
shineagencia-workflows <diego.shineagencia@gmail.com>
2026-04-30 13:48:18 -0500

    fix: update navigation links to use trailing slashes

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,12 +1,12 @@
 export const navBarLinksConst = [
   { name: "Inicio", path: "/" },
-  { name: "Nosotros", path: "/nosotros" },
-  { name: "Contacto", path: "/contacto" },
-  { name: "Blog", path: "/blog" },
+  { name: "Nosotros", path: "/nosotros/" },
+  { name: "Contacto", path: "/contacto/" },
+  { name: "Blog", path: "/blog/" },
 ] as const;
 
 export const servicesLinksConst = [
-  { name: "Estrategia de Marca", path: "/servicios/marca-personal" },
-  { name: "Diseño Web", path: "/servicios/diseno-web-estrategico" },
-  { name: "Rediseño web", path: "/servicios/rediseño-web-estrategico" },
+  { name: "Estrategia de Marca", path: "/servicios/marca-personal/" },
+  { name: "Diseño Web", path: "/servicios/diseno-web-estrategico/" },
+  { name: "Rediseño web", path: "/servicios/rediseno-web-estrategico/" },
 ]
